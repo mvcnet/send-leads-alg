4f7fbc6d20fe5843425e3773835a62b5fa751db4
mvcnet <105780594+mvcnet@users.noreply.github.com>
2025-04-15 22:14:28 -0300

    Update enviar-zapier.js

--- api/enviar-zapier.js
+++ api/enviar-zapier.js
@@ -1,11 +1,19 @@
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
   if (req.method !== 'POST') {
     return res.status(405).end("Método não permitido");
   }
 
   try {
     const zapierWebhook = "https://hooks.zapier.com/hooks/catch/15307898/20p7qcm/";
-    
+
     const resposta = await fetch(zapierWebhook, {
       method: "POST",
       headers: {
